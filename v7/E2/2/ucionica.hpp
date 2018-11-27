01dde2aa516f5f9819156e0b6f0c555304984aa3
l4v <ivos.jovan@protonmail.ch>
2018-11-27 17:09:31 +0100

    ZAPRAVO GOTOVA SEDMA

--- v7/E2/2/ucionica.hpp
+++ v7/E2/2/ucionica.hpp
@@ -7,16 +7,16 @@
 
 class Ucionica{
 private:
-    List<Osoba*> osobe;
+    List<Osoba> osobe;
     DinString naziv;
     int kapacitet;
 
 public:
     Ucionica(const DinString& n, int k) : naziv(n), kapacitet(k) {}
 
-    bool dodaj(Osoba& os){
+    bool dodaj(const Osoba& os){
         if(osobe.size() < kapacitet){
-            return osobe.add(osobe.size() + 1, &os);
+            return osobe.add(osobe.size() + 1, os);
         }
 
         return false;
@@ -26,12 +26,12 @@ public:
         if(osobe.size() == 0) {
             cout << endl << "Ucionica je prazna." << endl;
         } else {
-            Osoba* o;
+            Osoba o;
             cout << endl << "U ucionici su:" << endl;
 
             for (int i = 1; i <= osobe.size(); i++){
                 osobe.read(i, o);
-                o->predstaviSe();
+                o.predstaviSe();
             }
         }
     }
